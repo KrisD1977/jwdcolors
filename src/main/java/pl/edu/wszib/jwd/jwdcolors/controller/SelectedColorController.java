80ba0dbd6d7471c35bc7769d1c66680542b07dd7
KrisD1977 <derela@student.wszib.edu.pl>
2021-05-23 16:08:52 +0200

    widoki

--- src/main/java/pl/edu/wszib/jwd/jwdcolors/controller/SelectedColorController.java
+++ src/main/java/pl/edu/wszib/jwd/jwdcolors/controller/SelectedColorController.java
@@ -6,15 +6,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import pl.edu.wszib.jwd.jwdcolors.dao.SelectedColorDao;
+import pl.edu.wszib.jwd.jwdcolors.helper.ColorHelper;
 import pl.edu.wszib.jwd.jwdcolors.model.SelectedColor;
+import pl.edu.wszib.jwd.jwdcolors.service.SelectedColorService;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class SelectedColorController {
 
     @Autowired
-    SelectedColorDao selectedColorDao;
+    SelectedColorService selectedColorService;
 
     public static final String SELECT_COLOR_TITLE = "Wybierz kolor";
     public static final String DATA_TITLE = "Dane";
@@ -24,7 +29,7 @@ public class SelectedColorController {
     public String selectColorPage(@PathVariable(required = false) String color, Model model) {
 
         if(color != null) {
-            selectedColorDao.save(new SelectedColor(color, new Date()));
+            selectedColorService.save(color);
         }
 
 
@@ -39,5 +44,35 @@ public class SelectedColorController {
         model.addAttribute("title", SELECT_COLOR_TITLE);
         model.addAttribute("colors", colors);
         return "select";
+    }
+
+    @GetMapping("/data")
+    public String dataPage(Model model) {
+
+
+        model.addAttribute("title", DATA_TITLE);
+        model.addAttribute("selectedColors", selectedColorService.getAllData());
+        return "data";
+
+    }
+
+    @GetMapping("/stat")
+    public String statPage(Model model) {
+
+        List<SelectedColor> selectedColors = (List)selectedColorService.getAllData();
+        //można zmapwoać w inny sposób - iterując i zliczajęc - w pętli
+        Map<String, Long> dataMap = selectedColors.stream().collect(Collectors.groupingBy(SelectedColor::getColor, Collectors.counting()));
+
+        model.addAttribute("title", STAT_TITLE);
+        model.addAttribute("labels", dataMap.keySet());
+        model.addAttribute("values", dataMap.values());
+        model.addAttribute("backgrounds", ColorHelper.convertColors(dataMap.keySet()));
+
+        return "stat";
+
+    }
+    @GetMapping("/")
+    public String showStartPage() {
+        return "redirect:select";
     }
 }
